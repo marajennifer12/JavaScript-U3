f5048d6a3dbedd840ff0e5f4917350f6669a263b
marajennnifer12 <mariajenniferlopezlemus@gmail.com>
2023-04-24 14:31:01 -0600

    Arreglos

---
+++ Practica4.js
@@ -0,0 +1,41 @@
+const colors = ["Rojo", "Verde", "Azul", "Amarillo"]
+console.log(colors)
+
+console.log(colors[2])
+console.log(colors.length)
+
+
+const nums = [1,3,5,7,9,0]
+const doble = []
+for (let i = 0; i < nums.length; i++) {
+    doble.push(nums[i]*2)
+}
+console.log(nums)
+console.log(doble)
+
+
+//Sacar el promedio de los siguientes números
+const numbers = [5, 3, 4, 7, 2, 1, 9, 7, 7]
+let suma = 0
+
+for (let j = 0; j < numbers.length; j++) {
+    suma += numbers[j]
+}
+
+prom = suma/numbers.length
+console.log(prom)
+
+
+//Objetos
+const persona = {
+    nombre: "Rosario",
+    apellido: "Tijeras",
+    edad: 54,
+    estatura: 1.79
+}
+console.log(persona.nombre)
+
+persona.nombre = "Marianita"
+console.log(persona.nombre)
+
+//
